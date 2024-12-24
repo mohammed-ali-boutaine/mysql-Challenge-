88f98a9fb3a904929238a486e112e2e98bdc1c92
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2024-12-24 10:02:50 +0100

    finish first challenge

--- workshop-1.sql
+++ workshop-1.sql
@@ -50,8 +50,8 @@ create table Employe(
 );
 
 
--- partie 3-1
-
+-- Partie 3 : Manipulation des 
+-- 1
 insert into Client (CustomerFirstName,CustomerLastName,CustomerAddress,CustomerCity,CustomerPostCode,CustomerPhoneNumber)
 values ("mohammed ali","boutaine","hay nasser ","sale","1100","0650505050"),
 ("med","bout","hay nasser ","sale","1100","0650505050"),
@@ -63,13 +63,13 @@ values ("Anass", "Boutaib", "0654"),
 ("Asma", "Bichara", "5698");
 
 
-insert into Inventory (InventoryName, InventoryDescription)
+insert into Inventaire (InventoryName, InventoryDescription)
 values ("Central-inv", "The main inventory"),
 ("Additional-inv", "This is the secondary inventory"),
 ("New-inv", "Newly created inventory");
 
 
-insert into sale (CustomerID, InventoryID, EmployeeID, SaleDate, SaleQuantity, SaleUnitPrice)
+insert into Sale (CustomerID, InventoryID, EmployeeID, SaleDate, SaleQuantity, SaleUnitPrice)
 values (1, 1, 1, '2024-12-12', 3, 133),
 (2, 1, 2, "2024-12-03", 4, 152),
 (2, 2, 3, "2024-12-11", 1, 206),
@@ -78,7 +78,89 @@ values (1, 1, 1, '2024-12-12', 3, 133),
 
 -- partie 3-2
 
+-- 1
 select * from Client;
+
+-- 2
+select * , (SaleQuantity * SaleUnitPrice) as montants
+ from Sale
+order by montants desc;
+
+-- 3
+
+select * ,(Sale.SaleQuantity * Sale.SaleUnitPrice) as montants
+from Employe
+join Sale 
+on Employe.EmployeeID = Sale.EmployeeID;
+
+-------------------------------
+-- Partie 4 : Modifications des Tables
+
+-- 1
+alter table Client 
+add  CustomerEmail varchar(225);
+
+-- 2 
+alter table Client
+modify COLUMN  CustomerEmail  varchar(225) unique;
+
+-- 3 
+delete from Client
+where CustomerCity= "New York";
+
+-------------------------------
+-- Partie 5 : Requêtes Avancées
+-- 1
+select * from sale
+where  CURDATE() - saleDate < 30;
+
+-- 2
+select * from Client
+join Sale 
+on Client.CustomerID = Sale.CustomerID and SaleQuantity> 5;
+
+-- 3
+
+select  Inventaire.InventoryName,
+sum(Sale.SaleQuantity * Sale.SaleUnitPrice)
+from Sale
+join Inventaire
+on  sale.InventoryID = Inventaire.InventoryID
+GROUP BY Inventaire.InventoryName;
+
+
+-- Partie 6 : Bonus
+-- 1
+select * from CustomerSalesView;
+
+create view CustomerSalesView 
+as 
+select client.CustomerID,CustomerFirstName,CustomerLastName,SaleDate,InventoryName,SaleQuantity , (SaleQuantity * SaleUnitPrice) as TotalAmount from client 
+join sale 
+on sale.CustomerID = client.CustomerID
+join Inventaire
+on sale.InventoryID = Inventaire.InventoryID;
+
+-- 2
+
+delimiter //
+CREATE PROCEDURE pr_getVentes(id int )
+
+begin
+	select * FROM sale where CustomerID= id;
+
+end //
+
+
+
+
+
+
+
+
+
+
+
 
 
 
